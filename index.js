840fceba218c382188f04be76c3f455fde069d60
keyurj.medianv <keyur@medianv.com>
2023-03-07 10:54:30 +0530

    second commit

--- index.js
+++ index.js
@@ -9,24 +9,34 @@ let frmJson = [{
     class: 'form-control',
     label: 'Last Name'
 }, {
+    type: 'textarea',
+    id: 'txArea',
+    class: 'form-control',
+    label: 'Location'
+}, {
+    type: 'text',
+    id: 'txAddress',
+    class: 'form-control',
+    label: 'Address'
+}, {
     type: 'email',
     id: 'txEmail',
     class: 'form-control',
     label: 'Email'
-},{
+}, {
     type: 'password',
     id: 'txPassword',
     class: 'form-control',
     label: 'Password'
-},{
+}, {
     type: 'date',
     id: 'txtDob',
     class: 'form-control',
     label: 'Date Of Birth'
-},{
+}, {
     type: 'checkbox',
     id: 'btnCheck',
-    class: '',
+    class: 'form-check-input',
     label: 'Accept Terms & Condition'
 }, {
     type: 'button',
@@ -38,9 +48,13 @@ let frmJson = [{
 let frmHTML = '';
 for (let i = 0; i < frmJson.length; i++) {
     if (frmJson[i].type != 'button') {
-        frmHTML = `${frmHTML} <div class="form-group">
-        <label for="${frmJson[i].id}">${frmJson[i].label}</label>
-        <input type="${frmJson[i].type}" class="${frmJson[i].class}" id="${frmJson[i].id}"/>
+        frmHTML = `${frmHTML} <form class="row g-4">
+        <div class="col-md-4">
+          <label for="validationServer" class="form-label">${frmJson[i].label}</label>
+          <input type="${frmJson[i].type}" class="${frmJson[i].class} is-invalid" id="validationServer" value="" Required>
+          <div class="invalid-feedback">
+          Enter Valid Details
+          </div>
         </div>`;
     } else {
         frmHTML = `${frmHTML}
@@ -50,14 +64,14 @@ for (let i = 0; i < frmJson.length; i++) {
 }
 document.getElementById('dynamicFrm').innerHTML = frmHTML;
 // document.getElementById('jsonForm').innerText = JSON.stringify(frmJson);
-function onsaveclick(){
+function onsaveclick() {
     let frm = {
         firstName: document.getElementById('txFName').value,
         lastName: document.getElementById('txLName').value,
-        email:  document.getElementById('txEmail').value,
+        email: document.getElementById('txEmail').value,
         password: document.getElementById('txPassword').value,
         terms: document.getElementById('btnCheck').checked,
-        Date_of_birthday:document.getElementById('txtDob').value
+        Date_of_birthday: document.getElementById('txtDob').value
     };
     console.log(frm);
 }
