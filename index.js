6635bde36de4e167c43115e3dea143182e864342
Keyur Jayswal <119667449+Keyur7112022@users.noreply.github.com>
2023-03-09 16:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,38 @@ let frmJson = [{
     type: 'text',
     id: 'txFName',
     class: 'form-control',
-    label: 'First Name'
+    label: 'First Name',
+    validation: 'is-valid'
 }, {
     type: 'text',
     id: 'txLName',
     class: 'form-control',
-    label: 'Last Name'
+    label: 'Last Name',
+    validation: 'is-valid'
 }, {
     type: 'textarea',
     id: 'txArea',
     class: 'form-control',
-    label: 'Location'
+    label: 'Location',
+    validation: 'is-valid'
 }, {
     type: 'text',
     id: 'txAddress',
     class: 'form-control',
-    label: 'Address'
+    label: 'Address',
+    validation: 'is-valid'
 }, {
     type: 'email',
-    id: 'txEmail',
+    id: 'inputEmail',
     class: 'form-control',
-    label: 'Email'
+    label: 'Email',
+    validation: 'is-invalid'
 }, {
     type: 'password',
     id: 'txPassword',
     class: 'form-control',
-    label: 'Password'
+    label: 'Password',
+    validation: 'is-valid'
 }, {
     type: 'date',
     id: 'txtDob',
@@ -42,33 +48,39 @@ let frmJson = [{
     type: 'button',
     id: 'btnSave',
     class: 'btn btn-primary',
-    label: 'Save'
-}];
+    label: 'Submit'
+}
+];
 
 let frmHTML = '';
 for (let i = 0; i < frmJson.length; i++) {
     if (frmJson[i].type != 'button') {
-        frmHTML = `${frmHTML} <form class="row g-4">
-        <div class="col-md-4">
+        frmHTML = `${frmHTML} 
+        <form class="row-md-6">
+        <div class="col-md-4 align-items-center">
           <label for="validationServer" class="form-label">${frmJson[i].label}</label>
-          <input type="${frmJson[i].type}" class="${frmJson[i].class} is-invalid" id="validationServer" value="" Required>
+          <input type="${frmJson[i].type}" class="${frmJson[i].class} ${frmJson[i].validation}" id="validationServer" value="" Required>
           <div class="invalid-feedback">
           Enter Valid Details
+          </div>
+          <div class="valid-feedback">
+          Good 
           </div>
         </div>`;
     } else {
         frmHTML = `${frmHTML}
-        <button type="${frmJson[i].type}" class="${frmJson[i].class}" id="${frmJson[i].id}" onclick="onsaveclick()">${frmJson[i].label}</button>
-        `;
+        <button class="btn btn-primary btn-sm al gap-3" type="${frmJson[i].type}" onclick="onsaveclick()" >${frmJson[i].label}</button> `;
     }
 }
 document.getElementById('dynamicFrm').innerHTML = frmHTML;
 // document.getElementById('jsonForm').innerText = JSON.stringify(frmJson);
 function onsaveclick() {
-    let frm = {
+    var frm = {
         firstName: document.getElementById('txFName').value,
         lastName: document.getElementById('txLName').value,
-        email: document.getElementById('txEmail').value,
+        TxtArea: document.getElementById('txArea').value,
+        Address: document.getElementById('txAddress').value,
+        Email: document.getElementById('inputEmail').value,
         password: document.getElementById('txPassword').value,
         terms: document.getElementById('btnCheck').checked,
         Date_of_birthday: document.getElementById('txtDob').value
